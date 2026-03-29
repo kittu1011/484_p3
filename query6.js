2720273b25d45f4bd676c5a0a25f2f4d227e90e5
Kittu G <grewalg@umich.edu>
2026-03-28 21:44:25 -0400

    Implement Query 6

--- query6.js
+++ query6.js
@@ -4,8 +4,13 @@
 
 function find_average_friendcount(dbname) {
     db = db.getSiblingDB(dbname);
+    let total_users = 0;
+    let count_of_friends = 0;
 
+    db.users.find().forEach(function(user) {
+        ++total_users;
+        count_of_friends += user.friends.length;
+    });
     // TODO: calculate the average friend count
-
-    return 0;
+    return count_of_friends / total_users;
 }
