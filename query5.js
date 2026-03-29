7de0d9388be6e59e8df2c47a7f8a470434dd9a13
Kittu G <grewalg@umich.edu>
2026-03-28 22:58:27 -0400

    optimize q7

--- query5.js
+++ query5.js
@@ -8,30 +8,33 @@
 // {user1:userx1, user2:userx2, user3:userx3,...}
 
 function oldest_friend(dbname) {
+    let results = {};
+
     db = db.getSiblingDB(dbname);
     db.users.aggregate([
         {$unwind: "$friends"},
         {$project: { user_id: 1, friends: 1, _id: 0} },
         {$out: "flat_users"}
     ]);    // TODO: unwind friends
-    let results = {};
     // TODO: implement oldest friends
+    let friend_map = {};
+    db.flat_users.find().forEach(function(connection) {
+        let user_1 = connection.user_id;
+        let user_2 = connection.friends;
+        if (!friend_map[user_1]) friend_map[user_1] = [];
+        if (!friend_map[user_2]) friend_map[user_2] = []; 
+        friend_map[user_1].push(user_2);
+        friend_map[user_2].push(user_1);
+    });
+
     db.users.find().forEach(function(user) {
-        let user_friends = []
-        db.flat_users.find({"friends" : user.user_id}).forEach(function(connection) {
-            user_friends.push(connection.user_id);
-        })
+        let user_id = user.user_id;
+        if (!friend_map[user_id]) return;
 
-        db.flat_users.find({"user_id" : user.user_id}).forEach(function(connection) {
-            user_friends.push(connection.friends);
-        })
-
-        if (user_friends.length == 0) {
-            return;
-        }
+        let user_friends = friend_map[user_id]
 
         let oldest = db.users.find({user_id: {$in: user_friends}}).sort({YOB: 1, user_id: 1}).limit(1).next();
-        results[user.user_id] = oldest.user_id;
+        results[user_id] = oldest.user_id;
     });
     return results;
 }
